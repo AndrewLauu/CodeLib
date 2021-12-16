749014056a58b1859a6c4c9149a19a8af6baf471
Andrew Lau/Windows10 <Andrew.PF.Lau@Gmail.com>
2021-12-16 15:40:12 +0800

    modi coin

--- commemorativeCoins.py
+++ commemorativeCoins.py
@@ -1,3 +1,7 @@
+import logging
+import os
+import time
+
 import requests
 from lxml import etree
 import urllib.parse
@@ -6,11 +10,12 @@ import schedule
 
 
 def getICBCNews():
-    print('getting icbc news')
+    logging.debug('Getting icbc news...')
 
-    url = 'https://www.icbc.com.cn/ICBC/%E7%BA%AA%E5%BF%B5%E5%B8%81%E4%B8%93%E5%8C%BA/default.htm'
+    url = 'https://www.icbc.com.cn/ICBC/纪念币专区/default.htm'
     re = requests.get(url)
     html = etree.HTML(re.content.decode('utf8'))
+    logging.info(f'Response status is {re.status_code}')
 
     news = html.xpath('//span[@class="ChannelSummaryList-insty"]/a/@data-collecting-param')
     urls = html.xpath('//span[@class="ChannelSummaryList-insty"]/a/@href')
@@ -18,15 +23,18 @@ def getICBCNews():
     newUrl = 'https://www.icbc.com.cn'
     newNews = news[0]
     newUrl += urllib.parse.unquote(urls[0])
+    logging.info(f'Got latest news 《{newNews[:8]}...》')
+
     return newNews, newUrl
 
 
 def getPBCNews():
-    print('getting pbc news')
+    logging.debug('Getting pbc news...')
 
     url = 'http://www.pbc.gov.cn/huobijinyinju/147948/147964/22786/index2.html'
     re = requests.post(url)
     html = etree.HTML(re.content.decode('utf8'))
+    logging.info(f'Response status is {re.status_code}')
 
     news = html.xpath('//font[@class="newslist_style"]/a/@title')
     urls = html.xpath('//font[@class="newslist_style"]/a/@href')
@@ -34,6 +42,7 @@ def getPBCNews():
     newNews = news[0]
     newUrl = 'http://www.pbc.gov.cn'
     newUrl += urls[0]
+    logging.info(f'Got latest news 《{newNews[:8]}...》')
 
     return newNews, newUrl
 
@@ -45,12 +54,18 @@ def main():
         sendMsg = sendToast
     else:
         sendMsg = sendMail
+    logging.info(f'Message channel set as {NOTIFIER_CHANNEL}:{sendMsg.__name__}')
 
     icbcNews, icbcUrl = getICBCNews()
     pbcNews, pbcUrl = getPBCNews()
 
+    if not os.path.isfile('commemorativeCoins.json'):
+        open('commemorativeCoins.json', 'w').close()
+        logging.info('Did not find json log, created one.')
+
     with open('commemorativeCoins.json', 'r') as f:
         oldVersion = json.load(f)
+    logging.debug('Read json log.')
 
     newVersion = oldVersion.copy()
     newVersion['icbc']['news'] = icbcNews
@@ -62,13 +77,19 @@ def main():
         # send change
         content = (icbcNews, pbcNews)
         url = (icbcUrl, pbcUrl)
+        logging.info(f'Found new article {content}')
 
         sendMsg('纪念币更新', content, url)
+
         with open('commemorativeCoins.json', 'w') as f:
             json.dump(newVersion, f, ensure_ascii=False)
+
+        logging.info('Wrote to json log.')
+
     else:
         # send heartbeat
-        sendMsg('heartbeat', ('running',), ('',))
+        sendMsg('Heartbeat', ('plan is running, did not find new article',), ('',))
+        logging.info(f'did not find new article')
 
 
 def sendMail(title, contents, urls):
@@ -77,10 +98,9 @@ def sendMail(title, contents, urls):
     content = ''
     for url, con in zip(urls, contents):
         content += f'<a href="{url}">{con}</a>'
-
     yag = yagmail.SMTP('andrew_lauu@foxmail.com', 'sfrjjkcpkhhsbefa', host='smtp.qq.com')
     yag.send(to='andrew_lauu@126.com', subject=title, contents=content)
-    print(f'sent {title}')
+    logging.info(f'sent mail {title}')
 
 
 def sendToast(title, content, *args):
@@ -89,9 +109,18 @@ def sendToast(title, content, *args):
     toast = ToastNotifier()
     for c in content:
         toast.show_toast(title, c)
+        logging.info(f'Made toast {title}')
 
 
 if __name__ == '__main__':
-    schedule.every().day.at('10:30').do(main)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: <%(funcName)s> - %(message)s',
+                        datefmt='%Y-%d-%m %H:%M:%S')
+    logging.debug('Started process.')
+    schedule.every().day.at('10:00').do(main)
+
+    logging.debug('Scheduled plan.')
     while True:
+        plan = [repr(p) for p in schedule.get_jobs()]
+        logging.info(''.join(plan))
         schedule.run_pending()
+        time.sleep(60*60*2)
